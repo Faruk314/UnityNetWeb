42499b9d24899e2a6876d74638a1117f0fc8f7b9
Faruk Spahic <farukspahictz@gmail.com>
2023-05-02 14:10:52 +0200

    Added a new width, height and overflow to messenger.

--- src/modals/messenger/Messenger.tsx
+++ src/modals/messenger/Messenger.tsx
@@ -27,7 +27,6 @@ const Messenger = ({ setOpenMessages }: Props) => {
           `http://localhost:7000/api/messages/getUserConversations`
         );
 
-        console.log(response.data, "conv");
         setConversations(response.data);
       } catch (err) {}
     };
@@ -36,15 +35,20 @@ const Messenger = ({ setOpenMessages }: Props) => {
   }, [loggedUserInfo, messages]);
 
   return (
-    <div className="absolute top-[3.2rem] right-[-3rem] w-[19rem] bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-4 rounded-md z-20 text-black">
+    <div className="absolute top-[3.2rem] right-[-3rem] w-[20rem] bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-4 rounded-md z-20 text-black">
       <h2 className="font-bold text-[1.2rem]">Conversations</h2>
       <input
         type="text"
-        placeholder="Search Messenger"
+        placeholder="Search conversations"
         className="w-full rounded-full px-2 py-2 bg-gray-100 focus:outline-none my-4"
       />
 
-      <div>
+      <div className="flex flex-col space-y-2 overflow-y-auto h-[20rem]">
+        {conversations.length === 0 && (
+          <p className="text-center mt-2 text-blue-500">
+            You dont have any conversations
+          </p>
+        )}
         {conversations?.map((user, index) => (
           <Conversation
             key={index}
